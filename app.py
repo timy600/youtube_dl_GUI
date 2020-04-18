4ab5ca6befc64520bed233c5006bf492b6b0dc6c
Thibaut chevée <tchevee@hotmail.fr>
2020-04-18 16:32:14 +0200

    first test on youtube-dl

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from __future__ import unicode_literals
-
+import youtube_dl
 from tkinter import *
 
+url = "https://www.youtube.com/watch?v=kJQP7kiw5Fk"
+ydl_opts = {}
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    ydl.download([url])
+"""
 window = Tk()
 window.wm_title("Telecharger un son Youtube")
 window.mainloop()
+"""
