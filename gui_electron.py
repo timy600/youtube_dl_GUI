3df71d7660fb07f4c65d9c4a7d9da352c4b2981d
Thibaut chevée <tchevee@hotmail.fr>
2020-04-25 21:34:44 +0200

    import and launch server

--- gui_electron.py
+++ gui_electron.py
@@ -1,3 +1,12 @@
 # https://medium.com/@abulka/electron-python-4e8c807bfa5e
 
 # https://github.com/fyears/electron-python-example
+
+import zerorpc
+import gevent, signalport = 1234
+addr = 'tcp://127.0.0.1:' + port
+s = zerorpc.Server(PythermalApi())
+s.bind(addr)gevent.signal(signal.SIGTERM, s.stop)
+gevent.signal(signal.SIGINT, s.stop)  # ^C
+
+s.run()
